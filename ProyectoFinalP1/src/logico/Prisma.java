435d0c2df1d5d6b79a62ac65f46cb835febf1bc9
Work <Work@DESKTOP-II1IGJV>
2020-03-12 17:11:57 -0400

    Creando clase Prisma

---
+++ ProyectoFinalP1/src/logico/Prisma.java
@@ -0,0 +1,36 @@
+package logico;
+
+public abstract class Prisma {
+	protected float altura;
+	protected Vertice v1;
+	protected Vertice v2;
+	
+	public Prisma(float altura, Vertice v1, Vertice v2) {
+		super();
+		this.altura = altura;
+		this.v1 = v1;
+		this.v2 = v2;
+	}
+	
+	public float getAltura() {
+		return altura;
+	}
+	public void setAltura(float altura) {
+		this.altura = altura;
+	}
+	public Vertice getV1() {
+		return v1;
+	}
+	public void setV1(Vertice v1) {
+		this.v1 = v1;
+	}
+	public Vertice getV2() {
+		return v2;
+	}
+	public void setV2(Vertice v2) {
+		this.v2 = v2;
+	}
+	
+	
+
+}
