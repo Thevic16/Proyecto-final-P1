2b4b119a8806b5f0b1006c2d220c81ad7af7e4dc
victo <victo@LAPTOP-VICTORGOMEZ>
2020-03-12 18:33:24 -0400

    Agregandole metodos a la clase cuadrado para que esta genere todos los vertices del cuadrado

--- ProyectoFinalP1/src/logico/Cuadrado.java
+++ ProyectoFinalP1/src/logico/Cuadrado.java
@@ -24,5 +24,65 @@ public class Cuadrado extends Prisma {
 		return aux;
 	}
 	
+	
+	public ArrayList<Vertice> verticesCuadrado(Vertice v1, Vertice v2){
+		
+		ArrayList<Vertice>aux = new ArrayList<Vertice>();
+		aux.add(v1);
+		aux.add(v2);
+		//vertices que queremos determinar. 
+		Vertice v3 = null;
+		Vertice v4 = null;
+		
+		if(isOpuesto(v1,v2)) {
+			v3 = new Vertice(v1.getX(), v2.getY());
+			v4 = new Vertice(v2.getX(), v1.getY());
+		}
+		else if (isParalelo(v1,v2) && !(areIguales(v1, v2))) {
+			v3 = new Vertice(v1.getY(), v1.getX());
+			v4 = new Vertice(v2.getY(), v2.getX()); 
+		}
+		aux.add(v3);
+		aux.add(v4);
+		
+		return aux;
+		
+	}
+
+	private boolean isParalelo(Vertice v1, Vertice v2) {
+		// TODO Auto-generated method stub
+		boolean paralelo = false;
+		
+		if(v1.getX() == v2.getX()) {
+			paralelo = true;
+		}
+		if(v1.getY() == v2.getY()) {
+			paralelo = true;
+		}
+		
+		return paralelo;
+	}
+
+	
+	private boolean isOpuesto(Vertice v1, Vertice v2) {
+		boolean opuesto = false;
+		
+		if(v1.getX() == v2.getY() && v2.getX() == v1.getY()) {
+			opuesto = true;
+		}
+		
+		return opuesto;
+	}
+	
+	
+	public static boolean areIguales(Vertice v1, Vertice v2) {
+		boolean iguales = false;
+		
+		if(v1.getX() == v2.getX() && v1.getY() == v2.getY()) {
+			iguales = true;
+		}
+		
+		return iguales;
+	}
 
 }
