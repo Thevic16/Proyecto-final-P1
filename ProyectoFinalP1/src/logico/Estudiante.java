6f15950d5097b560e5d69b96f3ba252fe81e3dc9
Work <Work@DESKTOP-II1IGJV>
2020-04-03 22:23:20 -0400

    Cambiando nombre de parametros en constructor de "Profesor" y "Estudiante"

--- ProyectoFinalP1/src/logico/Estudiante.java
+++ ProyectoFinalP1/src/logico/Estudiante.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public class Estudiante extends Usuario {
 
-	public Estudiante(String nombre, String iD, String usuario, String password) {
-		super(nombre, iD, usuario, password);
+	public Estudiante(String nombre, String iD, String apellido, String password) {
+		super(nombre, iD, apellido, password);
 		// TODO Auto-generated constructor stub
 	}
 
--- ProyectoFinalP1/src/logico/Profesor.java
+++ ProyectoFinalP1/src/logico/Profesor.java
@@ -6,8 +6,8 @@ public class Profesor extends Usuario {
 	
 	private ArrayList<Estudiante>estudiantes; 
 
-	public Profesor(String nombre, String iD, String usuario, String password) {
-		super(nombre, iD, usuario, password);
+	public Profesor(String nombre, String iD, String apellido, String password) {
+		super(nombre, iD, apellido, password);
 		// TODO Auto-generated constructor stub
 		this.estudiantes = new ArrayList<Estudiante>();
 	}
