673156042abe116ef93230abe25ec13858fd8331
Work <Work@DESKTOP-II1IGJV>
2020-04-04 15:26:33 -0400

    Diseño base de ventana "Visualizar Figura"

---
+++ ProyectoFinalP1/src/visual/VisualizarFigura.java
@@ -0,0 +1,88 @@
+package visual;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JList;
+import javax.swing.JComboBox;
+import javax.swing.border.TitledBorder;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.border.EtchedBorder;
+import java.awt.Color;
+
+public class VisualizarFigura extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			VisualizarFigura dialog = new VisualizarFigura();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public VisualizarFigura() {
+		setTitle("Visualizar Figura");
+		setBounds(100, 100, 438, 363);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(null, "Informaci\u00F3n de Figura", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.setBounds(10, 11, 224, 269);
+		contentPanel.add(panel);
+		panel.setLayout(null);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"<Todas las Figuras>", "Cil\u00EDndro", "Cubo", "Cono", "Esfera", "Paralelep\u00EDpedo"}));
+		comboBox.setBounds(10, 22, 204, 22);
+		panel.add(comboBox);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Color de Figura", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel_2.setBounds(10, 70, 204, 188);
+		panel.add(panel_2);
+		panel_2.setLayout(null);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new TitledBorder(null, "Lista de Figuras", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_1.setBounds(244, 11, 168, 269);
+		contentPanel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JList list = new JList();
+		list.setBounds(10, 21, 148, 237);
+		panel_1.add(list);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+}
