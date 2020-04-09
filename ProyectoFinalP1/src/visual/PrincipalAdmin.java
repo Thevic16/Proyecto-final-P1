947f4fafe4c2065cfef9c4846832a6496e9daf2a
victo <victo@LAPTOP-VICTORGOMEZ>
2020-04-09 07:05:46 -0400

    Cerrar cesion ventana principal

--- ProyectoFinalP1/src/visual/PrincipalAdmin.java
+++ ProyectoFinalP1/src/visual/PrincipalAdmin.java
@@ -20,6 +20,9 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseAdapter;
 
 public class PrincipalAdmin extends JFrame {
 
@@ -130,6 +133,25 @@ public class PrincipalAdmin extends JFrame {
 			}
 		});
 		mnEstadisticas.add(mntmPromedioVolumenFiguras);
+		
+		JMenu mnNewMenu_2 = new JMenu("Cerrar sesion");
+		mnNewMenu_2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				try {
+					centro.setLoginAdmin(false);
+					guadarInstanceCentro(centro);
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				System.exit( 0 );
+			}
+		});
+
+
+
+		menuBar.add(mnNewMenu_2);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
